b6724aa9e23452d1b50aed4dd19fcc874003709a
Kanishk Tantia <ktantia@g.hmc.edu>
2017-08-01 16:47:01 -0700

    Added numpy functionality, still working on backprop

--- MLP.py
+++ MLP.py
@@ -4,7 +4,7 @@
 import math
 import random
 import numpy as np
-
+import copy
 
 #Activation functions and their derivatives
 
@@ -41,55 +41,56 @@ class MLP(object):
 		self.hiddenL = hiddenL
 
 		#Activation list
-		self.ai = [1.0] * self.inputN
-		self.ao = [1.0] * self.outputN
-		self.ah = [[1.0]*nodes for nodes in self.hiddenL]
+		self.ai = np.ones(self.inputN)
+		self.ah = np.asarray([np.ones(nodes) for nodes in self.hiddenL])
+		self.ao = np.ones(self.outputN)
 
 		#Random weights for input to hidden, hidden to hidden, hidden to output
 		self.wi = np.random.randn(self.inputN, self.hiddenL[0])
-		self.wh = [np.random.randn(self.hiddenL[x], self.hiddenL[x+1]) for x in range(len(self.hiddenL)-1)]
+		self.wh = np.asarray([np.random.randn(self.hiddenL[x], self.hiddenL[x+1]) for x in range(len(self.hiddenL)-1)])
 		self.wo = np.random.randn(self.hiddenL[-1], self.outputN)
 
 		#Arrays for weight changes
 		self.ci = np.zeros((self.inputN, self.hiddenL[0]))
-		self.ch = [np.zeros((self.hiddenL[x], self.hiddenL[x+1])) for x in range(len(self.hiddenL)-1)]
+		self.ch = np.asarray([np.zeros((self.hiddenL[x], self.hiddenL[x+1])) for x in range(len(self.hiddenL)-1)])
 		self.co - np.zeros((self.hiddenL[-1], self.outputN))
 
 
 	def feedForward(self, inputs):
 		if len(inputs) != self.inputN -1:
-			raise ValueError("Input has wrong length: " + str(len(inputs)))
+			raise ValueError("Input has wrong length: " + str(len(inputs)) + 
+			" as compared to: " + str(self.inputN - 1))
+
 		#Activations for the input layer
 		for i in range(self.inputN -1):
 			self.ai[i] = inputs[i]
 
 		#Activations for the first hidden layer (done separately to make loops easier)
-		for neuron in range(self.hiddenL[0]):
-			total = 0.0
-			for prevNeuron in range(self.inputN):
-				total += self.ai[prevNeuron]* self.wi[prevNeuron][neuron]
-			#calculate the activation in the hidden layer by going through activation function
-			self.ah[neuron] = sigmoid(total)
+		#calculate the activation in the hidden layer by going through activation function
+		self.ah = sigmoid(np.dot(self.ai, self.wi))
 		
 		#Activations for non hidden layer
 		#For every hidden layer after the first
 		for layer in range(1, len(self.hiddenL)):
-			#for every neuron in that hidden layer
-			for neuron in range(self.hiddenL[layer]):
-				total = 0.0
-				#total up the activations from the neurons of the previous layer times the connection weight
-				for prevNeuron in range(self.hiddenL[layer-1]):
-					total += self.ah[layer-1][prevNeuron] * self.wh[layer-1][prevNeuron][neuron]
-				self.ah[layer][neuron] = sigmoid(total)
+			self.ah[layer] = sigmoid(np.dot(self.ah[layer-1], self.wh[layer-1]))
 
 		#activations for output layer
-		for neuron in range(self.outputN):
-			total = 0.0
-			for prevNeuron in range(self.hiddenL[-1]):
-				total += self.ah[-1][prevNeuron] * self.wo[prevNeuron][neuron]
-			self.ao[neuron] = sigmoid(total)
+		self.ao = sigmoid(np.dot(self.ah[-1], self.wo))
 
-		return self.ao[:]
+		return self.ao
+
+	def errorCalc(self, targets):
+		'''
+		Calculates overall error in the MLP given the target vector
+		'''
+		if len(targets) != self.outputN:
+			raise ValueError("Given target length: " + str(len(targets))
+				+ " Expected: " + str(self.outputN))
+
+		error = targets - self.ao
+		error = error * error
+		error = error * 0.5
+		return sum(error)
 
 	def backPropagate(self, targets, learning):
 		"""
@@ -97,20 +98,19 @@ class MLP(object):
 		learning: The Learning rate for the backprop algorithm
 		returns updated weights and the current error
 		"""
+
 		if len(targets) != self.outputN:
-			raise valueError("Target and Output node mismatch")
-
-		#Calculate error values for output, delta indicates weight change direction
-
-		output_deltas = [0.0] * self.outputN
-
-		for neuron in range(self.outputN):
-			error = -(targets[neuron] - self.ao[neuron])
-			output_deltas[neuron] = dsigmoid(self.ao[neuron]) * error
-
+			raise ValueError("Given target length: " + str(len(targets))
+				+ " Expected: " + str(self.outputN))
+		
+		#Calculating the direction in which the output needs to change
+		#-(target-out) = out - target
+		errorAgainstOutput = self.ao - targets
+		outputDeltas = errorAgainstOutput * dsigmoid(self.ao)
+				
 		#calculating error values for last hidden layer
 		#Hidden deltas is a nested list which has deltas for all hidden layers
-		hidden_deltas = [[0.0] * x for x in self.hiddenL]
+		hidden_deltas = [np.zeros(x) for x in self.hiddenL]
 
 		for neuron in range(self.hiddenL[-1]):
 		 	error = 0.0
